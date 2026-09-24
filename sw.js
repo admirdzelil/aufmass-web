9e4a793b6a7929317bf2191f6b73ecfb8e010a17
Admir Dzelil <admirdzelil@gmail.com>
2026-09-24 12:38:44 +0200

    Beilagen nach TK filtern und AMBL mit Beilagen als PDF ausgeben

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
  *    trotzdem nachgeladen, solange eine Verbindung besteht.
  */
 
-const CACHE = 'aufmass-361af7ff';
+const CACHE = 'aufmass-05bbedae';
 const SHELL = ['/', '/index.html', '/manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
